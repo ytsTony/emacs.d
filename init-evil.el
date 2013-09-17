5302c82cb0100d00bc91967a118ea2b3de362f4d
ytsTony <colsy2@gmail.com>
2013-09-17 21:52:58 +0800

    bind :k in evil normal mode to kill current buffer and :wk for save and kill

--- init-evil.el
+++ init-evil.el
@@ -6,4 +6,18 @@
 (when (require 'paredit)
   (require-package 'evil-paredit))
 
+; customized ex cmd
+(defun kill-current-buffer()
+  "kill current buffer."
+  (kill-buffer (current-buffer)))
+
+(evil-ex-define-cmd "k" '(lambda ()
+                           (interactive)
+                           (kill-current-buffer)))
+
+(evil-ex-define-cmd "wk" '(lambda ()
+                            (interactive)
+                            (save-buffer)
+                            (kill-current-buffer)))
+
 (provide 'init-evil)
