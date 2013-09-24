a8b1bbeed5da3169de73473f4803fc2cf41a8aae
ytsTony <colsy2@gmail.com>
2013-09-24 23:08:38 +0800

    add evil ex cmd for ph project

--- init-ph.el
+++ init-ph.el
@@ -2,4 +2,9 @@
 
 (require 'ph)
 
+(when (require 'evil)
+  (evil-ex-define-cmd "pho" 'ph-project-open)
+  (evil-ex-define-cmd "phn" 'ph-project-new)
+  (evil-ex-define-cmd "phs" 'ph-project-switch))
+
 (provide 'init-ph)
